d803536fe8e62bd94000bde09a27d4a9f933a4f6
higinenggar <enggarhigin@gmail.com>
2022-01-19 08:55:16 +0700

    flutter_practice

---
+++ lib/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currIndex = 0;
+  final screens = [
+    Center(
+      child: Text(
+        'Home',
+        style: TextStyle(fontSize: 60, color: Color(0xFF7055d2)),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Chats',
+        style: TextStyle(fontSize: 60, color: Colors.blueAccent),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Feed',
+        style: TextStyle(fontSize: 60, color: Colors.teal),
+      ),
+    ),
+    Center(
+      child: Text(
+        'Calls',
+        style: TextStyle(fontSize: 60, color: Colors.pink),
+      ),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          title: Text('Bottom Navigation Bar'),
+          centerTitle: true,
+          backgroundColor: Color(0xFF7055d2),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.more_horiz),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: screens[currIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currIndex,
+          onTap: (index) => setState(() => currIndex = index),
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Color(0xFF7055d2),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
+              backgroundColor: Colors.blueAccent,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.feed),
+              label: 'Feed',
+              backgroundColor: Colors.teal,
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Calls',
+              backgroundColor: Colors.pink,
+            ),
+          ],
+        ),
+      );
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My First App',
+      home: HomePage(),
+    );
+  }
+}
